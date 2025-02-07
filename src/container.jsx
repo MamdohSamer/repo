8695f065dd9ef661788df7626758e1a2f7548f6d
MamdohSamer <143343786+MamdohSamer@users.noreply.github.com>
2025-02-07 17:39:13 +0200

    container.jsx

--- src/container.jsx
+++ src/container.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from 'c:/Users/Mamdouh samir/meat/src/header'
-import Home from 'c:/Users/Mamdouh samir/meat/src/Home'
-import About from 'c:/Users/Mamdouh samir/meat/src/About'
-import Services from'c:/Users/Mamdouh samir/meat/src/Services.jsx'
-import Contact from'c:/Users/Mamdouh samir/meat/src/Contact.jsx'
+import Header from 'src/header'
+import Home from 'src/Home'
+import About from 'src/About'
+import Services from'src/Services.jsx'
+import Contact from'src/Contact.jsx'
 import './container.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
